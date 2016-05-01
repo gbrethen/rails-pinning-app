46a77c0a565f70b74a590cc54fadd4d2f465d7d7
gbrethen <gbrethen@att.net>
2016-05-01 05:26:03 -0500

    Add create method

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,8 +9,29 @@ class PinsController < ApplicationController
   end
   
   def show_by_name
-	@pin = Pin.find_by_slug(params[:slug])
+	@pin = Pin.find_by_slug(params[:slug]) #slug is a field in the database so we can use a named url instead of the id
 	render :show
   end
   
+  def new
+	@pin = Pin.new
+  end
+  
+  def create
+	@pin = Pin.create(pin_params)
+	
+	if @pin.valid?
+		@pin.save
+		redirect_to pin_path(@pin)
+	else
+		@errors = @pin.errors
+		render :new
+	end
+  end
+  
+ private #every method below this is a private method
+  
+  def pin_params
+	params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+  end
 end
